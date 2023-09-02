26ce9be697a197651f456ffed82f56bd2d91341f
KishoreTejal <87982146+KishoreTejal@users.noreply.github.com>
2023-09-02 11:46:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+speak="";
+var synth = window.speechSynthesis;
+speak = objects[i].label;
+utterThi = new SpeechSynthesisUtterance(speak);
+synth.speak(utterThis);
+
+Status = "";
+input_text = "";
+
+function setup(){
+    canvas = createCanvas(300,290);
+    canvas.position(480,250);
+    video = createCapture(VIDEO);
+    video.size(300,290);
+    video.hide();
+}
+function start(){
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    input_text = document.getElementById("input_id").value;
+}
+function modelLoaded(){
+    console.log("Model_Loaded");
+    Status = true;
+}
+function draw(){
+    image(video,0,0,300,290);
+}
